dffb75870e751f3c29c40c80db141fda5df266f7
PythonLimited <Leonardomeitz@gmail.com>
2019-01-08 01:21:57 +0100

    remove now unused header and update dexpreopt

--- board/dexpreopt.mk
+++ board/dexpreopt.mk
@@ -1,6 +1,7 @@
 # Basic dexpreopt
 ifeq ($(HOST_OS),linux)
+
       #We need this otherwise WIFI causes a bootloop on each connect!
-      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT := false
 
 endif
